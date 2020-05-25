aeb53c6279c7b520ccb2e2236320467a88a39d0c
vokda <djing_djang@hotmail.com>
2020-05-26 01:11:16 +0200

    Minor fixes to deployment and services

--- services/name_gen/generate_name.php
+++ services/name_gen/generate_name.php
@@ -5,20 +5,31 @@ require "$root/templates/head.html";
 
 $fnl = $_GET['f_name_len'];
 $lnl = $_GET['l_name_len'];
+$n_n = $_GET['n_names'];
 
 chdir('name_generator/');
-$name = shell_exec("./generate_name.pl $fnl $lnl");
-$matches = [];
-preg_match('/name (.+) (.+)/', $name, $matches);
 
-echo "<h3> Name Generated </h3>";
-echo "<p>";
-echo $matches[1] . ' ' . $matches[2];
-echo "</p>";
+echo "<h3> Name(s) Generated </h3>";
+
+for($i = 0; $i < $n_n; $i++)
+{
+	if($n_n > 32)
+	{
+		echo "Nice try.";
+		break;
+	}
+	$name = shell_exec("./generate_name.pl $fnl $lnl");
+	$matches = [];
+	preg_match('/name (.+) (.+)/', $name, $matches);
+
+	echo "<p>";
+	echo $matches[1] . ' ' . $matches[2];
+	echo "</p>";
+}
 
 ?>
 <div>
-<button onclick='location.reload()'>Again</button>
+<button onclick='location.reload()'>Generate name(s) again</button>
 <button onclick='history.back()'>Change settings</button>
 </div>
 
--- services/name_gen/settings.php
+++ services/name_gen/settings.php
@@ -5,18 +5,22 @@ require "$root/templates/head.html";
 <body>
 	<form action='./generate_name.php' method='get'>
 		<p>
-			Enter length of first name: <input 
+			Length of first name: <input 
 			id='f_name_len' 
 			type=number 
 			min=0 
-			value=5
+			value=6
 			name='f_name_len'>
 		</p>
 		<p>
-			Enter length of last name: <input 
-			id='l_name_len' type=number min=0 value=5 name='l_name_len'>
+			Length of last name: <input 
+			id='l_name_len' type=number min=0 value=6 name='l_name_len'>
 		</p>
-		<button>post</button>
+		<p>
+			Number of names to generate: <input 
+			id='n_names' type=number min=1 value=1 max=32 name='n_names'>
+		</p>
+		<button>Generate Name</button>
 	</form>
 </body>
 <?php
