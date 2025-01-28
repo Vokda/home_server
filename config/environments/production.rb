2fb4312be907273bafae9455f39ee2d3772fd168
daniel <djing_djang@hotmail.com>
2025-01-28 19:02:38 +0000

    tmp solution is more permanent now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false # tmp solution to isp not allowing coms over 443
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
